69ff2810445f0ad3ea88c8364f4afabc4bd02833
j0shcd <joshua@cohendumani.com>
2024-11-23 15:44:03 +0100

    updated gitignore

--- target/debug/deps/rust_arbitrage_bot-9a7362c307918073.d
+++ target/debug/deps/rust_arbitrage_bot-9a7362c307918073.d
@@ -1,6 +1,6 @@
-/Users/joshuacohendumani/Documents/Documents /EPFL/Extra Curricular/BSA/Rust/working/rust_arbitrage_bot/target/debug/deps/rust_arbitrage_bot-9a7362c307918073: src/main.rs src/api_clients/mod.rs src/api_clients/binance.rs src/api_clients/okx.rs src/utils.rs src/arbitrage.rs
+/Users/joshuacohendumani/Documents/EPFL/Extra Curricular/BSA/Arbitrage Bot All Sparks/Rust/working/rust_arbitrage_bot/target/debug/deps/rust_arbitrage_bot-9a7362c307918073: src/main.rs src/api_clients/mod.rs src/api_clients/binance.rs src/api_clients/okx.rs src/utils.rs src/arbitrage.rs
 
-/Users/joshuacohendumani/Documents/Documents /EPFL/Extra Curricular/BSA/Rust/working/rust_arbitrage_bot/target/debug/deps/rust_arbitrage_bot-9a7362c307918073.d: src/main.rs src/api_clients/mod.rs src/api_clients/binance.rs src/api_clients/okx.rs src/utils.rs src/arbitrage.rs
+/Users/joshuacohendumani/Documents/EPFL/Extra Curricular/BSA/Arbitrage Bot All Sparks/Rust/working/rust_arbitrage_bot/target/debug/deps/rust_arbitrage_bot-9a7362c307918073.d: src/main.rs src/api_clients/mod.rs src/api_clients/binance.rs src/api_clients/okx.rs src/utils.rs src/arbitrage.rs
 
 src/main.rs:
 src/api_clients/mod.rs:
--- target/debug/deps/tokio_native_tls-ff1750f516a9820a.d
+++ target/debug/deps/tokio_native_tls-ff1750f516a9820a.d
@@ -1,7 +1,7 @@
-/Users/joshuacohendumani/Documents/Documents /EPFL/Extra Curricular/BSA/Rust/working/rust_arbitrage_bot/target/debug/deps/tokio_native_tls-ff1750f516a9820a.rmeta: /Users/joshuacohendumani/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tokio-native-tls-0.3.1/src/lib.rs
+/Users/joshuacohendumani/Documents/EPFL/Extra Curricular/BSA/Arbitrage Bot All Sparks/Rust/working/rust_arbitrage_bot/target/debug/deps/tokio_native_tls-ff1750f516a9820a.rmeta: /Users/joshuacohendumani/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tokio-native-tls-0.3.1/src/lib.rs
 
-/Users/joshuacohendumani/Documents/Documents /EPFL/Extra Curricular/BSA/Rust/working/rust_arbitrage_bot/target/debug/deps/libtokio_native_tls-ff1750f516a9820a.rlib: /Users/joshuacohendumani/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tokio-native-tls-0.3.1/src/lib.rs
+/Users/joshuacohendumani/Documents/EPFL/Extra Curricular/BSA/Arbitrage Bot All Sparks/Rust/working/rust_arbitrage_bot/target/debug/deps/libtokio_native_tls-ff1750f516a9820a.rlib: /Users/joshuacohendumani/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tokio-native-tls-0.3.1/src/lib.rs
 
-/Users/joshuacohendumani/Documents/Documents /EPFL/Extra Curricular/BSA/Rust/working/rust_arbitrage_bot/target/debug/deps/tokio_native_tls-ff1750f516a9820a.d: /Users/joshuacohendumani/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tokio-native-tls-0.3.1/src/lib.rs
+/Users/joshuacohendumani/Documents/EPFL/Extra Curricular/BSA/Arbitrage Bot All Sparks/Rust/working/rust_arbitrage_bot/target/debug/deps/tokio_native_tls-ff1750f516a9820a.d: /Users/joshuacohendumani/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tokio-native-tls-0.3.1/src/lib.rs
 
 /Users/joshuacohendumani/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tokio-native-tls-0.3.1/src/lib.rs:
